b85094c89dc2bf447f065e2a1cf10f0674e3505d
Ƭєℓє Ƙιηg <mohmadrsol81@gmail.com>
2016-02-11 22:45:42 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -239,16 +239,14 @@ function create_config( )
     sudo_users = {151590044},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Creed bot 2.3
+    about_text = [[TeleKing bot 2.3
     
      Hello my Good friends 
      
     ‼️ this bot is made by : @creed_is_dead
    〰〰〰〰〰〰〰〰
    ߔࠀ   our admins are : 
-   ߔࠀ   @sorblack_creed
-   ߔࠀ   @amircc_creed
-   ߔࠀ   @aria_creed
+   ߔࠀ   @teelegram356
    〰〰〰〰〰〰〰〰
   ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :
    تمامی درخواست ها و همه ی انتقادات و حرفاتونو با دستور زیر بفرستین به ما
